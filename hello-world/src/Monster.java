679f9b871a0f66857c6833eeff1aef61b10cdd8b
matthewvolk <volkmattj@gmail.com>
2018-07-26 20:00:48 -0700

    add monster file forgot in last commit

---
+++ hello-world/src/Monster.java
@@ -0,0 +1,195 @@
+import java.util.Arrays;
+
+// A file cannot contain more than one PUBLIC class
+public class Monster {
+	
+	static char[][] battleBoard = new char[10][10];
+	public static void buildBattleBoard() {
+		for(char[] row : battleBoard) {
+			Arrays.fill(row, '*');
+		}
+	}
+	
+	public static void redrawBoard() {
+		int k = 1;
+		while (k <= 31) { System.out.print('='); k++; }
+		System.out.println();
+		
+		for (int i = 0; i < battleBoard.length; i++) {
+			for (int j = 0; j < battleBoard[i].length; j++) {
+				System.out.print("|" + battleBoard[i][j] + "|");
+			}
+			System.out.println();
+		}
+		
+		k = 1;
+		while (k <= 31) { System.out.print('='); k++; }
+		System.out.println();
+	}
+	
+	public final String TOMBSTONE = "Here lies a dead monster.";
+	
+	private int health = 500;
+	private int attack = 20;
+	private int movement = 2;
+	private boolean alive = true;
+	
+	// You should have as few public fields as possible
+	public String name = "Big Monster";
+	public char nameChar1 = 'B';
+	public int xPosition = 0;
+	public int yPosition = 0;
+	public static int numOfMonsters = 0;
+	
+	public int getAttack() {
+		return attack;
+	}
+	
+	public int getMovement() {
+		return movement;
+	}
+	
+	public int getHealth() {
+		return health;
+	}
+	
+	public boolean getAlive() {
+		return alive;
+	}
+	
+	public void setHealth(int amountToDecreaseHealth) {
+		health = health - amountToDecreaseHealth;
+		if (health < 0) {
+			alive = false;
+		}
+	}
+	
+	public void setHealth(double amountToDecreaseHealth) {
+		int intAmountToDecreaseHealth = (int) amountToDecreaseHealth; 
+		health = health - intAmountToDecreaseHealth;
+		if (health < 0) {
+			alive = false;
+		}
+	}
+	
+	public void moveMonster(Monster[] monster, int arrayItemIndex) {
+		
+		boolean isSpaceOpen = true;
+		
+		// Define max board size
+		int maxXBoardSpace = battleBoard.length - 1;
+		int maxYBoardSpace = battleBoard[0].length - 1;
+		
+		while(isSpaceOpen) {
+			int randMoveDirection = (int) (Math.random() * 4);
+			int randMoveDistance = (int) (Math.random() * (this.getMovement() + 1));
+			
+			System.out.println(randMoveDistance + " " + randMoveDirection);
+			
+			// Erase the monster's current position by replacing it with a star
+			battleBoard[this.yPosition][this.xPosition] = '*';
+			
+			if (randMoveDirection == 0) {
+				if ((this.yPosition - randMoveDistance) < 0) {
+					this.yPosition = 0;
+				} else {
+					this.yPosition = randMoveDistance;
+				}
+			} else if (randMoveDirection == 1) {
+				if ((this.xPosition + randMoveDistance) > maxXBoardSpace) {
+					this.xPosition = maxXBoardSpace;
+				} else {
+					this.xPosition = xPosition + randMoveDistance;
+				}
+			} else if (randMoveDirection == 2) {
+				if ((this.yPosition + randMoveDistance) > maxYBoardSpace) {
+					this.yPosition = maxYBoardSpace;
+				} else {
+					this.yPosition = yPosition + randMoveDistance;
+				}
+			} else {
+				if ((this.xPosition - randMoveDistance) < 0) {
+					this.xPosition = 0;
+				} else {
+					this.xPosition = xPosition - randMoveDistance;
+				}	
+			}
+			
+			// When moving a monster, first thing we need to think is "is the target location to move to occupied?"
+			for (int i = 0; i < monster.length; i++) {
+				if (i == arrayItemIndex) {
+					continue;
+				}
+				if (onMySpace(monster, i, arrayItemIndex)) {
+					isSpaceOpen = true;
+					break;
+				} else {
+					isSpaceOpen = false;
+				}
+			}
+		} // END OF WHILE LOOP
+		
+		battleBoard[this.yPosition][this.xPosition] = this.nameChar1;
+		
+	} // END OF moveMonster
+	
+	public boolean onMySpace (Monster[] monster, int indexToChk1, int indexToChk2) {
+		if ((monster[indexToChk1].xPosition) == (monster[indexToChk2].xPosition) && (monster[indexToChk1].yPosition) == (monster[indexToChk2].yPosition)) {
+			return true;
+		} else {
+			return false;
+		}
+	} // END OF onMySpace
+	
+	/*
+	 * Class Constructor
+	 * Code that is invoked when class is instantiated with 'new' operator
+	 * 
+	 */
+	public Monster(int health, int attack, int movement, String name) {
+		this.health = health; // 'this' is a reference to the actual object's variables
+		this.attack = attack;
+		this.movement = movement;
+		this.name = name;
+		
+		int maxXBoardSpace = battleBoard.length - 1;
+		int maxYBoardSpace = battleBoard[0].length - 1;
+		
+		int randNumX, randNumY;
+		
+		// Using a do loop because we don't want any kind of condition to prevent the loop from happening at least once
+		do {
+			randNumX = (int) (Math.random() * maxXBoardSpace);
+			randNumY = (int) (Math.random() * maxYBoardSpace);
+		} while (battleBoard[randNumX][randNumY] != '*');
+		
+		this.xPosition = randNumX;
+		this.yPosition = randNumY;
+		
+		this.nameChar1 = this.name.charAt(0);
+		
+		battleBoard[this.xPosition][this.yPosition] = this.nameChar1;
+		
+		numOfMonsters++;
+		
+	}
+	
+	// Default Constructor
+	public Monster() {
+		
+	}
+	
+	public Monster(int newHealth) {
+		health = newHealth;
+	}
+	
+	public Monster(int newHealth, int newAttack) {
+		this(newHealth);
+		attack = newAttack;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
