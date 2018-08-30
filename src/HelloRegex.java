7b84b68f2bb610b03158eb1256eb38f0ecc56e66
matthewvolk <volkmattj@gmail.com>
2018-08-29 19:34:27 -0700

    add sample strings for regex

---
+++ src/HelloRegex.java
@@ -0,0 +1,10 @@
+import java.util.regex.*;
+
+public class HelloRegex {
+	
+	public static void main(String[] args) {
+		String longString = " Matt Volk CA 12345 PA (949)466-3267 johnsmith@gmail.com ";
+		String strangeString = " 1Z aaa **** *** {{{ {{ { ";
+	}
+	
+}
